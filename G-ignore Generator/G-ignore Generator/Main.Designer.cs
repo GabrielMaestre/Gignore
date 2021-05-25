8e655340a7860e24916a42dabaee21204e8791a1
Gabriel Moreira <gmaestre002@gmail.com>
2021-05-25 01:02:53 -0300

    Update - v0.2alpha

--- G-ignore Generator/G-ignore Generator/Main.Designer.cs
+++ G-ignore Generator/G-ignore Generator/Main.Designer.cs
@@ -29,21 +29,105 @@ namespace G_ignore_Generator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblTitleA = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblTitleA
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.lblTitleA.AutoSize = true;
+            this.lblTitleA.Location = new System.Drawing.Point(112, 90);
+            this.lblTitleA.Name = "lblTitleA";
+            this.lblTitleA.Size = new System.Drawing.Size(161, 20);
+            this.lblTitleA.TabIndex = 0;
+            this.lblTitleA.Text = "Select Your Repository!";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(25, 113);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(334, 27);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.Location = new System.Drawing.Point(139, 306);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(106, 43);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(25, 157);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(334, 133);
+            this.txtResult.TabIndex = 3;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOptions.Location = new System.Drawing.Point(297, 1);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(86, 34);
+            this.btnOptions.TabIndex = 4;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.UseVisualStyleBackColor = true;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.Location = new System.Drawing.Point(130, 2);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(124, 85);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 5;
+            this.picLogo.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnOptions);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblTitleA);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(251)))), ((int)(((byte)(252)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "G-ignore Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitleA;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.PictureBox picLogo;
     }
 }
 
--- G-ignore Generator/G-ignore Generator/Properties/AssemblyInfo.cs
+++ G-ignore Generator/G-ignore Generator/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
 [assembly: AssemblyTitle("G-ignore Generator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Git-ignore Generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PINGUIN'S COMPANY")]
 [assembly: AssemblyProduct("G-ignore Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
